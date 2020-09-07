03227250285502c0c1f9a38562a8e324a90087e3
thomasfregene <fregzythomas@gmail.com>
2020-09-07 12:56:54 +0100

    created a car class and a create car function -script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-window.addEventListener('load', ()=> document.querySelector('.preloader').classList.add('hidePreloader'))
+window.addEventListener('load', ()=> document.querySelector('.preloader').classList.add('hidePreloader'));
+
+//immediately invoked function using ES6 arrow function
+const createCars = (()=>{
+    //car data
+    const cars = [];
+
+    //car class
+    class Car{
+        constructor(make,country,img,special,model,price,type,trans,gas){
+            this.make = make;
+            this.country = country;
+            this.img = img;
+            this.special = special;
+            this.model = model;
+            this.price = price;
+            this.type = type;
+            this.trans = trans;
+            this.gas = gas
+        }
+    }
+
+    //car creation function with default values
+    function makeCar(make,country,img ='img/car-default.jpeg',special=true,model='new model',price=10000,type='sedan',trans='automatic',gas='50'){
+        const car = new Car(make,country,img,special,model,price,type,trans,gas);
+        cars.push(car);
+    }
+    
+    //produce car function
+})();
