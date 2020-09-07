a2d39f2348ed80c7e80978e09d2152b49ae306da
thomasfregene <fregzythomas@gmail.com>
2020-09-07 14:39:55 +0100

    added display car module

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', ()=> document.querySelector('.preloader').classList.add('hidePreloader'));
 
 //immediately invoked function using ES6 arrow function
-const createCars = (()=>{
+const CreateCars = (()=>{
     //car data
     const cars = [];
 
@@ -30,20 +30,64 @@ const createCars = (()=>{
     function produceCars(){
         makeCar('chevy', 'american')
         makeCar('mercedes', 'german', 'img/car-german-1.jpeg', true)
-        makeCar('mercedes', 'german', 'img/car-german-2.jpeg', true)
-        makeCar('mercedes', 'german', 'img/car-german-3.jpeg', false, 'some model')
-        makeCar('mercedes', 'german', 'img/car-german-4.jpeg', undefined, 'other model')
+        makeCar('bmw', 'german', 'img/car-german-2.jpeg', true)
+        makeCar('bmw', 'german', 'img/car-german-3.jpeg', false, 'some model')
+        makeCar('bmw', 'german', 'img/car-german-4.jpeg', undefined, 'other model')
         makeCar('mercedes', 'german', 'img/car-german-5.jpeg', false)
-        makeCar('chevy','american','img/car-amercian-1.jpeg')
-        makeCar('chevy','american','img/car-amercian-2.jpeg', false)
-        makeCar('chevy','american','img/car-amercian-3.jpeg', false)
-        makeCar('chevy','american','img/car-amercian-4.jpeg', false)
-        makeCar('chevy','american','img/car-amercian-5.jpeg', false)
+        makeCar('chevy','american','img/car-american-1.jpeg')
+        makeCar('chevy','american','img/car-american-2.jpeg', false)
+        makeCar('chevy','american','img/car-american-3.jpeg', false)
+        makeCar('chevy','american','img/car-american-4.jpeg', false)
+        makeCar('chevy','american','img/car-american-5.jpeg', false)
     }
     produceCars();
-    console.log(cars);
+    //console.log(cars);
 
     //special cars
     const specialCars = cars.filter(car=>car.special===true);
-    console.log(specialCars);
+    //console.log(specialCars);
+
+    return{
+        cars,
+        specialCars
+    }
 })();
+
+// display special cars Module
+const DisplaySpecialCars =((CreateCars)=>{
+    const specialCars = CreateCars.specialCars;
+    // console.log(CreateCars.specialCars);
+
+    //targeting the featured car section with css class
+    const info = document.querySelector('.featured-info');
+
+    //document loaded event
+    document.addEventListener('DOMContentLoaded', ()=>{
+        info.innerHTML = '';
+
+        let data = '';
+
+        specialCars.forEach(item=>{
+            data += ` <!-- single item -->
+            <div class="featured-item my-3 d-flex p-2 text-capitalize align-items-baseline flex-wrap">
+                <span data-img="${item.img}" class="featured-icon mr-2">
+                    <i class="fas fa-car"></i>
+                </span>
+                <h5 class="font-weight-bold mx-1">${item.make}</h5>
+                <h5 class="mx-1">${item.model}</h5>
+            </div>
+            <!-- end of single item -->`
+        })
+        info.innerHTML = data;
+    })
+
+    //change img
+    info.addEventListener('click', (event)=>{
+        if(event.target.parentElement.classList.contains('featured-icon')){
+            //targeting the data-img in icon span
+            const img = event.target.parentElement.dataset.img;
+            document.querySelector('.featured-photo').src = img;
+        }
+    })
+    
+})(CreateCars);
